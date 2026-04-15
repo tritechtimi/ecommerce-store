dcb8d18c13483616e100ce15fffbd92fabe48503
tritechtimi <triangleofscholars@gmail.com>
2026-04-15 01:50:56 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-hereimport { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY";
+const supabaseUrl = "https://fnzwectsjwzmstyyurpl.supabase.co";
+
+const supabaseKey =   "sb_publishable_tycgtpPWLx3YfWZk8IE2Lw_oZYpZo3t";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
